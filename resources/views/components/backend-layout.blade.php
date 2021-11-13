1b292d3519e1837cf278fcafa87f378c53349bc2
Johnson Sebire <johnson@manifestghana.com>
2021-11-13 12:04:07 +0000

    Removed Emotality Tawk Package | Replaced with Tawk.to Script

--- resources/views/components/backend-layout.blade.php
+++ resources/views/components/backend-layout.blade.php
@@ -408,8 +408,19 @@
    <!-- ======= End BEGIN PAGE LEVEL PLUGINS/CUSTOM SCRIPTS ======= -->
    @livewireScripts
 
-   {{ \TawkTo::widgetCode("https://tawk.to/chat/554b259ef82948372f4615b6/1e9lrte10") }}
+   <script type="text/javascript" defer>
 
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+      var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+      s1.async=true;
+      s1.src='https://embed.tawk.to/554b259ef82948372f4615b6/1e9lrte10';
+      s1.charset='UTF-8';
+      s1.setAttribute('crossorigin','*');
+      s0.parentNode.insertBefore(s1,s0);
+      })();
+      </script>
+      <!--End of Tawk.to Script-->
 </body>
 
 </html>
--- resources/views/components/frontendlayout.blade.php
+++ resources/views/components/frontendlayout.blade.php
@@ -23,19 +23,7 @@
 <link href="{{asset("frontend/css/theme.css")}}" rel="stylesheet" type="text/css">
 <!--Start of Tawk.to Script-->
 
-{{-- <script type="text/javascript" defer>
-@csrf
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/554b259ef82948372f4615b6/1e9lrte10';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-    </script>
-    <!--End of Tawk.to Script--> --}}
+
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.2/dist/alpine.min.js" defer></script>
 
 
@@ -144,7 +132,19 @@
 <!-- Global Init -->
 <script src="{{asset('frontend/js/global.js')}}"></script>
 
-{{ \TawkTo::widgetCode() }}
+<script type="text/javascript" defer>
+
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/554b259ef82948372f4615b6/1e9lrte10';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
+    <!--End of Tawk.to Script-->
 
 </body>
 </html>
