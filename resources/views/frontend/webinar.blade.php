e06226d30942ebf8730a795fa1d8b2af8c16e0cb
Johnson Sebire <johnson@manifestghana.com>
2022-01-25 12:36:28 +0000

    Updated Webinar Details

--- resources/views/frontend/webinar.blade.php
+++ resources/views/frontend/webinar.blade.php
@@ -9,7 +9,7 @@
 			<div class="offset-lg-3 col-lg-6">
 				<h1 class="text-white m-b-10"><span style="font-size:14px"> FREE WEBINAR | FRIDAY 28TH JANUARY 2022 | 10 AM</span> <br />Convert More Prospects into Buyers/Hot Leads</h1>
 				<p class="text-white m-b-40">How to Create a Highly Converting Landing Page</p>
-				<a href="#" class="button button-primary transition-3d-hover">Register</a>
+				<a href="https://forms.zoho.com/manifestmultimedia/form/Signup" class="button button-primary transition-3d-hover">Register</a>
 			</div>
 		</div>
 	</div>
@@ -19,14 +19,14 @@
 </section>
 <!-- Welcome End -->
 	<!-- Page Content Start -->
-	<section class="m-t-n100 position-relative z-i-2 p-b-90">
+	<section class="m-t-n100 position-relative p-b-90 z-i-2" style="margin-top:-200px !important">
 		<div class="container">
 			<div class="row">
 				<!-- Item Start -->
 				<div class="col-lg-12 col-md-6" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s">
-					<a  class="box overflow-hidden rounded imgfix-animate h-500 d-block m-b-30">
+					<a  class="box overflow-hidden rounded imgfix-animate h-580 d-block m-b-30">
 						
-						<video controls autoplay loop id="vid">
+						<video controls autoplay loop id="vid" width="100%">
 							<source src="{{asset('frontend/videos/webinar/featured.mp4')}}" alt="Webinar Video" type="video/mp4">
 							Your browser does not support this video format. 
 						</video>
@@ -40,7 +40,72 @@
 		</div>
 	</section>
 
-	<script>
-		document.getElementById('vid').play();
-	</script>
+	<section class="m-t-n100 position-relative p-b-90 z-i-2" >
+		<div class="container">
+
+			<div class="d-flex align-items-center">
+	
+				<div class="offset-lg-1 col-lg-8">
+					<div class="header-badge m-b-15">Free Webinar</div>
+					<h2 class="m-b-10">Webinar Details</h2>
+					<p class="m-b-40">Turning your prospects into buyers is how you make money. There are different ways to achieve this and it will be different for every business type. 
+						<br /> <br />
+						Unfortunately not many people have been able to take advantage of their digital presence to 
+						increase ROI on their investment(s). 
+						<br /><br /> 
+						In this webinar we will be taking you through the steps of creating a highly converting landing page for your product or service which would enable you reduce bounce rates and increase revenue. 
+					<br /><br /> Our goal has always been to help you maximize the use of your website. Below are a few things we would cover in this webinar:</p>
+
+					<div class="row">
+						<div class="col-md-6">
+							<div class="icon-list d-flex align-items-center m-b-15">
+								<div class="icon">
+									<i class="fa fa-check"></i>
+								</div>
+								<p>Intro to Landing Pages</p>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="icon-list d-flex align-items-center m-b-15">
+								<div class="icon">
+									<i class="fa fa-check"></i>
+								</div>
+								<p>Increase Conversions</p>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="icon-list d-flex align-items-center m-b-15">
+								<div class="icon">
+									<i class="fa fa-check"></i>
+								</div>
+								<p>Reduce Bounce Rates</p>
+							</div>
+						</div>
+						<div class="col-md-6">
+							<div class="icon-list d-flex align-items-center m-b-15">
+								<div class="icon">
+									<i class="fa fa-check"></i>
+								</div>
+								<p>Increase Revenue</p>
+							</div>
+						</div>
+					</div>
+					
+				<a href="https://forms.zoho.com/manifestmultimedia/form/Signup" class="button button-primary transition-3d-hover">Save Your Spot</a>
+		
+				</div>
+				<div class="col-lg-4">
+					<img src="{{asset('frontend/images/webinar/featuredpresenter.png')}}" width="40%" heigth="40%" alt="Presenter"> 
+					<br />
+					<h3> Presenter: </h3>
+					<h3> Johnson Sebire </h3>
+					<span> Founder, Manifest Group </span> 
+					<p> <a href="https://fb.me/sebirejohnson"> Facebook </a></p> 
+					<p> <a href="https://www.linkedin.com/in/johnsonsebire/"> LinkedIn </a></p> 
+					<p> <a href="https://johnsonsebire.com"> Website </a></p> 
+				</div>
+			</div>
+		</div>
+	</section>
+	
 </x-frontendlayout>
