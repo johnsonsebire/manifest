ede0b8eeca4224a91e5434c5905205c45e7e46f4
Johnson Sebire <johnson@manifestghana.com>
2022-07-13 15:10:17 +0000

    Updated Homepage Content

--- resources/views/frontend/new.blade.php
+++ resources/views/frontend/new.blade.php
@@ -13,35 +13,36 @@
                 <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="service-item service-style-three mb-30 wow fadeInUp" data-wow-delay=".3s">
                         <div class="icon">
-                            <img src="{{asset('pixlab/images/icon/icon-4.png')}}" alt="">
+                            <img src="{{asset('pixlab/images/icon/icon-12.png')}}" height="70px" alt="Web Development">
                         </div>
                         <div class="text">
-                            <h3 class="title"><a href="{{url('')}}">Mobile App Development</a></h3>
-                            <p>We produce robust, mobile apps for a wide range of companies as a leading mobile apps development company.</p>
+                            <h3 class="title"><a href="{{url('')}}">Website Development</a></h3>
+                            <p>We build robust & responsive websites that drive digital transformation with high ROI for our cherished customers. </p>
                             <a href="{{url('')}}" class="btn-link-two">Read More</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12">
-                    <div class="service-item service-style-three mb-30 wow fadeInUp" data-wow-delay=".15s">
+                    <div class="service-item service-style-three mb-30 wow fadeInUp" data-wow-delay=".3s">
                         <div class="icon">
-                            <img src="{{asset('pixlab/images/icon/icon-1.png')}}" alt="">
+                            <img src="{{asset('pixlab/images/icon/icon-4.png')}}" alt="">
                         </div>
                         <div class="text">
-                            <h3 class="title"><a href="{{url('')}}">Creative Design</a></h3>
-                            <p>Sed ut perspiciatis unde omnisy natus error volupt cusantium</p>
+                            <h3 class="title"><a href="{{url('')}}">App Development</a></h3>
+                            <p>We produce quality customer-centric mobile & web applications that are matched to your needs. </p>
                             <a href="{{url('')}}" class="btn-link-two">Read More</a>
                         </div>
                     </div>
                 </div>
+                
                 <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="service-item service-style-three mb-30 wow fadeInUp" data-wow-delay=".2s">
                         <div class="icon">
                             <img src="{{asset('pixlab/images/icon/icon-2.png')}}" alt="">
                         </div>
                         <div class="text">
-                            <h3 class="title"><a href="{{url('')}}">Marketing Strategy</a></h3>
-                            <p>Sed ut perspiciatis unde omnisy natus error volupt cusantium</p>
+                            <h3 class="title"><a href="{{url('')}}">SAP Consulting</a></h3>
+                            <p>We carry out a detailed examination of the client's current process & improve on internal workflow processes.</p>
                             <a href="{{url('')}}" class="btn-link-two">Read More</a>
                         </div>
                     </div>
@@ -52,8 +53,8 @@
                             <img src="{{asset('pixlab/images/icon/icon-3.png')}}" alt="">
                         </div>
                         <div class="text">
-                            <h3 class="title"><a href="{{url('')}}">Design Strategy</a></h3>
-                            <p>Sed ut perspiciatis unde omnisy natus error volupt cusantium</p>
+                            <h3 class="title"><a href="{{url('')}}">Digital Marketing</a></h3>
+                            <p>We carve out winning digital marketing solutions for clients to reach their target audiences & thrive.</p>
                             <a href="{{url('')}}" class="btn-link-two">Read More</a>
                         </div>
                     </div>
@@ -66,12 +67,24 @@
                         </div>
                         <div class="text">
                             <h3 class="title"><a href="{{url('')}}">SEO Optimization</a></h3>
-                            <p>Sed ut perspiciatis unde omnisy natus error volupt cusantium</p>
+                            <p>We create custom SEO campigns to help your most valuable audience find you online.</p>
                             <a href="{{url('')}}" class="btn-link-two">Read More</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="service-item service-style-three mb-30 wow fadeInUp" data-wow-delay=".15s">
+                        <div class="icon">
+                            <img src="{{asset('pixlab/images/icon/icon-1.png')}}" alt="">
+                        </div>
+                        <div class="text">
+                            <h3 class="title"><a href="{{url('')}}">Branding & Advertising</a></h3>
+                            <p>We create strong brands that leave a lasting impression & produce brand loyalists for your organization!</p>
+                            <a href="{{url('')}}" class="btn-link-two">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                {{-- <div class="col-lg-4 col-md-6 col-sm-12">
                     <div class="service-item service-style-three mb-30 wow fadeInUp" data-wow-delay=".4s">
                         <div class="icon">
                             <img src="{{asset('pixlab/images/icon/icon-6.png')}}" alt="">
@@ -82,7 +95,7 @@
                             <a href="{{url('')}}" class="btn-link-two btn-link-two">Read More</a>
                         </div>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </div>
