4747b1ea939ae1be647362ea017a4cfab1e6ac70
Johnson Sebire <johnson@manifestghana.com>
2022-07-18 21:39:26 +0000

    Updated Off Canvas Panel

--- resources/views/components/pixlab-off-canvas-panel.blade.php
+++ resources/views/components/pixlab-off-canvas-panel.blade.php
@@ -4,7 +4,7 @@
     <div class="panel-overlay"></div>
     <div class="offcanvas-panel-inner">
         <div class="panel-logo">
-            <a href="index.html"><img src="{{asset('frontend/images/logo/logo.png')}}" alt="Logo"></a>
+            <a href="{{url('/')}}"><img src="{{asset('frontend/images/logo/logo.png')}}" alt="Logo"></a>
         </div>
         <div class="about-us">
             <h5 class="panel-widget-title">{{__('Why Us?')}}</h5>
@@ -21,16 +21,16 @@
                 </li> --}}
                 <li>
                     <i class="fal fa-envelope-open"></i>
-                    <a href="mailto:hello@barky.com">{{__('info@manifestghana.com')}}</a>
-                    <a href="mailto:info@barky.com">{{__('business@manifestghana.com')}}</a>
+                    <a href="mailto:info@manifestghana.com">{{__('info@manifestghana.com')}}</a>
+                    <a href="mailto:business@manifestghana.com">{{__('business@manifestghana.com')}}</a>
                 </li>
                 <li>
                     <i class="fal fa-phone"></i>
-                    <a href="tel:(312)-895-9800">(+233) 549-539417</a>
-                    <a href="tel:(312)-895-9888">(+233) 504-259908</a>
+                    <a href="tel:(+233)-549-539417">(+233) 303-958782</a>
+                    <a href="tel:(+233)-504-259908">(+233) 504-259908</a>
                 </li>
             </ul>
         </div>
-        <a href="#" class="panel-close"><i class="fal fa-times"></i></a>
+        <a href="{{url('#')}}" class="panel-close"><i class="fal fa-times"></i></a>
     </div>
 </div><!--====== offcanvas-panel ======-->
